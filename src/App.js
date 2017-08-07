48ef2d650840187982d2d0d0ed59977d03e9f552
Peter Wang <happypeter1983@gmail.com>
2017-08-07 20:10:24 +0800

    react-router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react'
 import './App.css'
+import Home from './Home'
+import Post from './Post'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        hello
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/post" component={Post} />
+          </Switch>
+        </Router>
       </div>
     );
   }
---
+++ src/Post.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Post extends Component {
+  render(){
+    return(
+      <div className="post">
+        Post
+      </div>
+    )
+  }
+}
+
+export default Post

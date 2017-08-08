2bf3197db16f8f09408ef4dbab401ec1c3bcae3e
Peter Wang <happypeter1983@gmail.com>
2017-08-08 14:21:00 +0800

    submit comment

--- src/CommentBox.js
+++ src/CommentBox.js
@@ -2,10 +2,44 @@ import React, { Component } from 'react'
 import './comment-box.css'
 
 class CommentBox extends Component {
+
+  state = {
+    comments: [
+      'hello1',
+      'hello2'
+    ]
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    let comments = this.state.comments.slice()
+    let content = this.commentInput.value
+    comments.push(content)
+    this.setState({
+      comments
+    })
+    this.commentInput.value = ''
+  }
   render(){
+
+    let commentList = this.state.comments.slice().reverse().map((item, i) => (
+      <li key={i}>{item}</li>
+    ))
+
+    let commentForm = (
+      <form onSubmit={this.handleSubmit}>
+        <input type="text" ref={value => this.commentInput = value} />
+        <button type="submit">提交</button>
+      </form>
+    )
     return(
       <div className="comment-box">
-        CommentBox
+        <div className="form">
+          {commentForm}
+        </div>
+        <ul className="comment-list">
+          {commentList}
+        </ul>
       </div>
     )
   }
